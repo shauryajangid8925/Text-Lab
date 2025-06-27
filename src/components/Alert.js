236ed3f965f83fa1cc08010ea56db04e54f3b763
Shaurya Jangid <shauryajangid8925@gmail.com>
2025-06-27 04:59:25 -0700

    Add Alert.js component for app alert messages design.
    Created Alert.js to design and manage alert messages in the app, enhancing feedback to users.

---
+++ src/components/Alert.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Alert(props) {
+  return (
+    <div style={{ height: '25px' }}>
+      {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+        <strong>🔔 {props.alert.type}</strong> {props.alert.msg}
+      </div>}
+    </div>
+  )
+}
